36cc74907c44c5173da5372a97caea83126b550f
sbalazote <sbalazote@gmail.com>
2016-11-04 17:20:59 -0300

    #agrego funcionalidad contacto en pag. ppal

---
+++ Web/Controllers/ContactController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult> HandleForm(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Correo de: {0} ({1})</p><p>Mensaje:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("winning.com@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress(contact.Email);  // replace with valid value
+                message.Subject = contact.Name;
+                message.Body = string.Format(body, contact.Name, contact.Email, contact.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "winning.com@gmail.com",  // replace with valid value
+                        Password = "sanlorenzoARG84"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    //return RedirectToAction("Sent");
+                }
+            }
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+    }
+}
---
+++ Web/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class Contact
+    {
+        [Required (ErrorMessage = "Ingrese un Nombre, por favor.")]
+        [Display(Name = "Nombre Completo")]
+        public string Name { get; set; }
+
+        [Required (ErrorMessage = "Ingrese un Telefono, por favor.")]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Telefono")]
+        public string Phone { get; set; }
+
+        [Required (ErrorMessage = "Ingrese un Correo Electronico, por favor.")]
+        [DataType(DataType.EmailAddress), EmailAddress(ErrorMessage = "El {0} no es un e-mail valido.")]
+        [Display(Name = "Correo Electronico")]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage = "Ingrese un Mensaje de Consulta, por favor.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Mensaje")]
+        [StringLength(100, ErrorMessage = "El {0} debe contener al menos {2} caracteres.", MinimumLength = 6)]
+        public string Message { get; set; }
+    }
+}
